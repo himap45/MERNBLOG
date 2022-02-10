756a44b52382a8864e21ab53c47dc589f85f8c2c
CYBDM_LAP_03 <admin@CYBDMLAP2.domeserver.local.domain>
2022-02-10 18:22:42 +0530

    git

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import Home from "./pages/home/Home";
+import Post from "./components/post/Post";
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import TopBar from "./components/topbar/TopBar";
+import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import SinglePost from "./components/singlepost/SinglePost";
+import { useContext } from "react";
+import { Context } from "./context/Context";
+
+function App() {
+  const { user } = useContext(Context);
+  return (
+    <Router>
+      <>
+     <TopBar />
+     <Routes>
+        <Route path="/" element={<Home/>} ></Route>
+       <Route path="/register" element={user ? <Home />:<Register/>}></Route>
+       <Route path="/post/:id" element={<SinglePost/>}></Route>
+       <Route path="/login" element={user ? <Home/>:<Login/>}></Route>
+       <Route path="/write" element={user ? <Write/>:<Login/>}></Route>
+       <Route path="/settings" element={user ? <Settings/>:<Login/>}></Route>
+        </Routes>
+        </>
+    </Router>  
+  );
+}
+export default App;
---
+++ client/src/components/sidebar/Sidebar.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import './sidebar.css'
+
+function Sidebar(props) {
+  const [cats, setCats] = useState([]);
+
+  useEffect(() => {
+    const getCats = async () => {
+      const res = await axios.get("/categories");
+      setCats(res.data);
+    };
+    getCats();
+  }, []);
+    return (
+        <div className='sidebar'>
+             <div className="sidebarItem">
+        <span className="sidebarTitle">FOLLOW US</span>
+        <div className="sidebarSocial">
+          <i className="sidebarIcon fab fa-facebook-square"></i>
+          <i className="sidebarIcon fab fa-instagram-square"></i>
+          <i className="sidebarIcon fab fa-pinterest-square"></i>
+          <i className="sidebarIcon fab fa-twitter-square"></i>
+        </div>
+        </div>
+        </div>
+    );
+}
+
+export default Sidebar;
